7493aba1d651d662f21f767aca1c269ca7ee7d69
Eduardojcruz <eduardo.jose.cruz@outlook.com>
2023-05-06 18:55:41 -0300

    Gerador Senha

--- Gerador_Senha.py
+++ Gerador_Senha.py
@@ -8,3 +8,8 @@ import string
 def password_generator(len_pass = 10):
     ascii_options = string.ascii_letters
     number_options  = string.digits
+    punt_options = string.punctuation
+    options = ascii_options +  number_options + punt_options
+
+    for digig in range(0, len_pass):
+        []
