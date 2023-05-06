486feaec16e965adaf89fd7cd65df86dcbe0a66e
Eduardojcruz <eduardo.jose.cruz@outlook.com>
2023-05-06 19:07:54 -0300

    Gerador Senha

--- Gerador_Senha.py
+++ Gerador_Senha.py
@@ -5,11 +5,29 @@ Gerador de senhas
 import random
 import string
 
-def password_generator(len_pass = 10):
+def password_generator(len_pass):
     ascii_options = string.ascii_letters
     number_options  = string.digits
     punt_options = string.punctuation
     options = ascii_options +  number_options + punt_options
 
-    for digig in range(0, len_pass):
-        []
+    password_user = ""
+    for digit in range(0, len_pass):
+        digit = random.choice(options)
+        password_user =  password_user + digit
+    return password_user
+
+
+choice_user = input("Quantos digitos na senha?")
+
+if choice_user.isdigit():
+    choice_user = int(choice_user)
+    response  = password_generator(len_pass = choice_user)
+    print("Senha gerada {}".format(response))
+
+else:
+    print("Valor invalido ")
+    quit()
+
+
+
