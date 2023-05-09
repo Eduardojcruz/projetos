bdadec15920dd1995466973b34063e082a3a42f8
Eduardojcruz <eduardo.jose.cruz@outlook.com>
2023-05-08 21:20:53 -0300

    Jogo pedra,papel,tesoura

--- Pedra_papel_tesoura.py
+++ Pedra_papel_tesoura.py
@@ -12,12 +12,31 @@ options = ["r", "t", "p"]
 
 
 while True:
-    user_choice = input("Escolha R(Pedra)/T(Tesoura)/P(Papel) ou Q para sair.").lower()
-
+    user_choice = input("\n R(Pedra) \n T(Tesoura) \n P(Papel) \n Q para sair. \n Escolha: ").lower()
+    
     if user_choice == 'q':
         break
 
     if user_choice not in options:
         continue
 
-    computer_choice = random.randint(0, 2)
+    comp_choice = random.randint(0, 2)
+
+    compu_options = options[comp_choice]
+
+    print(f"O computador escolheu: {compu_options}" )
+
+    if user_choice == compu_options:
+        print("Empate!")
+
+    elif user_choice == "r" and compu_options == "t":
+        print("Você ganhou!")
+        user_points = user_points + 1
+
+    elif user_choice == "p" and compu_options == "r":
+        print("Você ganhou!")
+        user_points = user_points + 1
+
+    elif user_choice == "t" and compu_options == "p":
+        print("Você ganhou!")
+        user_points = user_points + 1
