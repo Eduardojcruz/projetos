25016b200d3f5b6cfb663811730eedcfbeee3a25
Eduardojcruz <eduardo.jose.cruz@outlook.com>
2023-05-08 21:25:57 -0300

    Jogo pedra,papel,tesoura

--- Pedra_papel_tesoura.py
+++ Pedra_papel_tesoura.py
@@ -13,7 +13,7 @@ options = ["r", "t", "p"]
 
 while True:
     user_choice = input("\n R(Pedra) \n T(Tesoura) \n P(Papel) \n Q para sair. \n Escolha: ").lower()
-    
+
     if user_choice == 'q':
         break
 
@@ -23,20 +23,42 @@ while True:
     comp_choice = random.randint(0, 2)
 
     compu_options = options[comp_choice]
-
-    print(f"O computador escolheu: {compu_options}" )
+    print("-" * 20 )
+    print(f" O computador escolheu: {compu_options}" )
 
     if user_choice == compu_options:
-        print("Empate!")
-
+        print(" Empate!")
+        print("-" * 20 )
     elif user_choice == "r" and compu_options == "t":
-        print("Você ganhou!")
+        print(" Você ganhou!")
+        print("-" * 20 )
         user_points = user_points + 1
 
     elif user_choice == "p" and compu_options == "r":
-        print("Você ganhou!")
+        print( "Você ganhou!")
+        print("-" * 20 )
         user_points = user_points + 1
 
     elif user_choice == "t" and compu_options == "p":
-        print("Você ganhou!")
+        print(" Você ganhou!")
+        print("-" * 20 )
         user_points = user_points + 1
+
+    else:
+        print(" Você perdeu!")
+        print("-" * 20 )
+        computer_points = computer_points + 1
+
+print("-" * 20 )
+print("Sua pontuação: " + str(user_points))
+print("Pontuação do Computador: " + str(computer_points))
+
+if computer_points > user_points:
+    print("Derrota!!!!")
+    print("-" * 20 )
+elif computer_points == user_points:
+    print("Empate")
+    print("-" * 20 )
+else:
+    print("Vitória!!")
+    print("-" * 20 )
